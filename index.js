5570ebfb6ab7f86adc79fe971d8661eac6b1a6b4
ToMMaaY <thuynh629@gmail.com>
2019-01-10 10:59:47 -0800

    Make port dynamic

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const server = require('./api/server.js');
+const server = require("./api/server.js");
 
-server.listen(5000, () => console.log('server on port 5k'));
+// enable dynamic ports for hosting provider
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log("server on port 5k"));
